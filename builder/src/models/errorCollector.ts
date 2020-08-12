845080ea4cfb0b22f499170809b941ce6c314ad3
Clément Bourgoin <c@iwzr.fr>
2020-08-12 12:36:42 +0200

    Fix models error collector returning error from non-custom assertions

--- builder/src/models/errorCollector.ts
+++ builder/src/models/errorCollector.ts
@@ -1,8 +1,13 @@
 import { Model } from 'objectmodel';
 
+/**
+ * Throw readable error message from objectmodel module custom assertion
+ */
 Model.prototype.errorCollector = function errorCollector(errors) {
   errors.forEach((error) => {
-    const message = error.message.match(/returned Error: (.*) for value/m)[1];
+    const matches = error.message.match(/returned Error: (.*) for value/m);
+    const message = (matches && matches[1]) || error.message;
+
     throw new Error(message);
   });
 };
