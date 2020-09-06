ae62842923a3807819d5ff4ae43d7d523233de49
Clément Bourgoin <c@iwzr.fr>
2020-09-06 21:27:14 +0200

    Add english and french translations

---
+++ template/src/translations/fr.js
@@ -0,0 +1,6 @@
+export default [
+  { key: 'Powered by', text: 'Propulsé par' },
+  { key: 'by', text: 'de' },
+  { key: 'Search', text: 'Rechercher' },
+  { key: 'Search…', text: 'Recherche…' },
+];
